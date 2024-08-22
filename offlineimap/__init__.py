5c9b83bedccda6cd888be8a1771d7b77bfb38417
Etienne Buira <etienne.buira@free.fr>
2024-08-22 11:16:21 +0200

    Copyright: We are currently in 2024
    Signed-off-by: Etienne Buira <etienne.buira@free.fr>

--- offlineimap/__init__.py
+++ offlineimap/__init__.py
@@ -3,7 +3,7 @@ __all__ = ['OfflineImap']
 __productname__ = 'IMAPMirror'
 # Expecting trailing "-rcN" or "" for stable releases.
 __version__ = "8.0.0"
-__copyright__ = "Copyright 2002-2021 John Goerzen & contributors"
+__copyright__ = "Copyright 2002-2024 John Goerzen & contributors"
 __author__ = "John Goerzen"
 __description__ = "Disconnected Universal IMAP Mail Synchronization/Reader Support"
 __license__ = "Licensed under the GNU GPL v2 or any later version (with an OpenSSL exception)"
